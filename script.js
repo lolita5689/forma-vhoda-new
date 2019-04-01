e75f2b21ec688257e2fbe2af4e31ad0ce45a1974
Svetlana <47456804+lolita5689@users.noreply.github.com>
2019-04-01 22:59:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+   /*if (h.value === value.toLowerCase()){
+      login.style.border = "2px solid red";
+      return false;
+   }
+   if(!g.value){
+      parol.style.border = "2px solid red";
+      return false;
+   }
+   return true;
+*/
+     
+    function myFunction(){
+    
+      var login = document.getElementById("login").value
+      var pass = document.getElementById("parol").value
+      
+      if (){ login.pattern != "^[а-яёa-z]+$"  
+
+          
+      }
+      else{
+          alert("вы залогинились")
+      }
+  }
+  
